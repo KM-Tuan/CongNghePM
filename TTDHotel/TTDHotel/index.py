a8164180002151fef6ff0d7f4071f797c5460b4b
kieuminhtuan2112003@gmail.com <kieuminhtuan2112003@gmail.com>
2024-12-06 19:10:52 +0700

    Successful Recovery

---
+++ TTDHotel/TTDHotel/index.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from unicodedata import category
+
+import dao
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    q = request.args.get("q")
+    cate_id = request.args.get("category_id")
+    categories = dao.load_categories()
+    products = dao.load_products(q=q, cate_id=cate_id)
+    return render_template('index.html', categories=categories, products=products)
+
+
+@app.route('/products/<int:id>')
+def details(id):
+
+    return render_template('product-details.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
