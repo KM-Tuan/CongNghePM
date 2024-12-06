01be77f8ab4a12142c931fa058d4a16451c3f64b
Cyter <tu2004latoi@gmail.com>
2024-12-06 20:28:47 +0700

    pro vai

--- TTDHotel/TTDHotel/index.py
+++ TTDHotel/TTDHotel/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from unicodedata import category
 
 import dao
@@ -19,7 +19,10 @@ def details(id):
 
     return render_template('product-details.html')
 
-
+@app.route('/home')
+def home():
+    categories = dao.load_categories()
+    return render_template('home.html', categories=categories)
 
 if __name__ == "__main__":
     app.run(debug=True)
