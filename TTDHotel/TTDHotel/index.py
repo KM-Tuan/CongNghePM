a7ae51d7d114cfbcd4c4bd99f7db64b03fd8bfcc
Cyter <tu2004latoi@gmail.com>
2024-12-20 00:21:50 +0700

    tú đưa lên

--- TTDHotel/TTDHotel/index.py
+++ TTDHotel/TTDHotel/index.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session, url_for
 
 import dao
 from TTDHotel.TTDHotel import app
@@ -6,22 +6,29 @@ from TTDHotel.TTDHotel import app
 
 @app.route('/home')
 def index():
+    logged_in = session.get('logged_in',False)
     q = request.args.get("q")
     cate_id = request.args.get("category_id")
     products = dao.load_products(q=q, cate_id=cate_id)
-    return render_template('index.html', products=products)
+    session['next'] = request.url
+    return render_template('index.html', products=products, logged_in=logged_in)
 
 
 @app.route('/products/<int:id>')
 def details(id):
     product = dao.load_product_by_id(id)
-    return render_template('product-details.html', product=product)
+    logged_in = session.get('logged_in',False)
+    if not logged_in:
+        session['next'] = request.url
+
+    return render_template('product-details.html', product=product, logged_in=logged_in)
 
 @app.route('/')
 def categories():
+    logged_in = session.get('logged_in',False)
     category_id = request.args.get('category_id')
     products = dao.load_product_by_category_id(category_id)
-    return render_template('index.html', products=products)
+    return render_template('index.html', products=products, logged_in=logged_in)
 
 
 
@@ -32,9 +39,16 @@ def login_my_user():
         password = request.form.get('password')
         user = dao.auth_user(username, password)
         if user:
-            return redirect('/')
+            session['logged_in'] = True
+            next_page = session.get('next')
+            return redirect(next_page)
 
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    return redirect(request.referrer)
 
 
 @app.route('/')
