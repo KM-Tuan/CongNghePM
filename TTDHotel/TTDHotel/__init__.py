96602eb8d08bbbec456a3c09bece612e27baf977
kieuminhtuan2112003@gmail.com <kieuminhtuan2112003@gmail.com>
2024-12-22 14:55:32 +0700

    commit

--- TTDHotel/TTDHotel/__init__.py
+++ TTDHotel/TTDHotel/__init__.py
@@ -7,6 +7,6 @@ app.secret_key = 'secret_key'
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/hoteldb?charset=utf8mb4" % quote('Admin@123')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config['PAGE_SIZE'] = 8
+app.config['PAGE_SIZE'] = 2
 
 db = SQLAlchemy(app)
