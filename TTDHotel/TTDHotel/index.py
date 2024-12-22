51121a2d35a92aa6e6f16423df3150a6e3dad4b9
NVD576 <duw3210@gmail.com>
2024-12-22 16:09:41 +0700

    phân trang

--- TTDHotel/TTDHotel/index.py
+++ TTDHotel/TTDHotel/index.py
@@ -128,7 +128,9 @@ def authorize():
     session['profile'] = user_info
     session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
     session['logged_in'] = True
-    return redirect('/')
+    next_page = session.get('next')
+    return redirect(next_page)
+
 
 
 @app.route('/logout_google')
@@ -173,7 +175,10 @@ def authorize_facebook():
     # Lưu thông tin vào session
     session['profile'] = user_info
     session['logged_in'] = True
-    return redirect('/')
+
+    next_page = session.get('next')
+    return redirect(next_page)
+
 
 
 @app.route('/logout_facebook')
