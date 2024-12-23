f56677ad48a786dd655614d38bb24f5d3852e47c
NVD576 <duw3210@gmail.com>
2024-12-23 22:27:25 +0700

    phân trang

--- TTDHotel/TTDHotel/admin.py
+++ TTDHotel/TTDHotel/admin.py
@@ -3,7 +3,7 @@ from flask_admin.contrib.sqla import ModelView
 from models import Category, Product
 from TTDHotel.TTDHotel import app, db
 
-admin = Admin(app, name="TTDHotel", template_mode="boostrap4")
+admin = Admin(app, name="TTDHotel", template_mode="bootstrap4")
 
 admin.add_view(ModelView(Category, db.session))
 admin.add_view(ModelView(Product, db.session))
