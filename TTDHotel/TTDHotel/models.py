2084191084a07df1934612e977370491b1c34011
kieuminhtuan2112003@gmail.com <kieuminhtuan2112003@gmail.com>
2024-12-26 16:29:46 +0700

    commit

--- TTDHotel/TTDHotel/models.py
+++ TTDHotel/TTDHotel/models.py
@@ -67,38 +67,38 @@ class Rating(db.Model):
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
-        c1 = Category(name="Phòng VIP")
-        c2 = Category(name="Phòng Đôi")
-        c3 = Category(name="Phòng Đơn")
-        l1 = Location(name="TP Hồ Chí Minh")
-        l2 = Location(name="Hà Nội")
-        l3 = Location(name="Vũng Tàu")
-        db.session.add_all([c1, c2, c3, l1, l2, l3])
-        db.session.commit()
-        import json
-        # Lấy category và location bằng cách sử dụng session.get() thay vì query.get()
-        with open('data/products.json', encoding='utf-8') as f:
-            products = json.load(f)
-            for p in products:
-                category = db.session.get(Category, p['category_id'])  # Sử dụng session.get()
-                location = db.session.get(Location, p['location_id'])  # Sử dụng session.get()
-                prod = Product(
-                    name=p['name'],
-                    address=p['address'],
-                    distance=p['distance'],
-                    description=p['description'],
-                    price=p['price'],
-                    image=p['image'],
-                    category=category,  # Sử dụng đối tượng Category thay vì ID
-                    location=location  # Sử dụng đối tượng Location thay vì ID
-                )
-                db.session.add(prod)
-
-        db.session.commit()
-
-        import hashlib
-        u = User(name="KieuThanhDuc", phone="0987654321", username="admin", password= str(hashlib.md5("123".encode('utf-8')).hexdigest()))
-        db.session.add(u)
-        db.session.commit()
-        # pass
+        # db.create_all()
+        # c1 = Category(name="Phòng VIP")
+        # c2 = Category(name="Phòng Đôi")
+        # c3 = Category(name="Phòng Đơn")
+        # l1 = Location(name="TP Hồ Chí Minh")
+        # l2 = Location(name="Hà Nội")
+        # l3 = Location(name="Vũng Tàu")
+        # db.session.add_all([c1, c2, c3, l1, l2, l3])
+        # db.session.commit()
+        # import json
+        # # Lấy category và location bằng cách sử dụng session.get() thay vì query.get()
+        # with open('data/products.json', encoding='utf-8') as f:
+        #     products = json.load(f)
+        #     for p in products:
+        #         category = db.session.get(Category, p['category_id'])  # Sử dụng session.get()
+        #         location = db.session.get(Location, p['location_id'])  # Sử dụng session.get()
+        #         prod = Product(
+        #             name=p['name'],
+        #             address=p['address'],
+        #             distance=p['distance'],
+        #             description=p['description'],
+        #             price=p['price'],
+        #             image=p['image'],
+        #             category=category,  # Sử dụng đối tượng Category thay vì ID
+        #             location=location  # Sử dụng đối tượng Location thay vì ID
+        #         )
+        #         db.session.add(prod)
+        #
+        # db.session.commit()
+        #
+        # import hashlib
+        # u = User(name="KieuThanhDuc", phone="0987654321", username="admin", password= str(hashlib.md5("123".encode('utf-8')).hexdigest()))
+        # db.session.add(u)
+        # db.session.commit()
+        pass
