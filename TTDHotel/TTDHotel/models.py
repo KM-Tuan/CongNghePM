34e3f371323c8fcfb304c14eba293bad8e5a9cc9
Cyter <tu2004latoi@gmail.com>
2024-12-26 16:29:15 +0700

    tú đưa lên

--- TTDHotel/TTDHotel/models.py
+++ TTDHotel/TTDHotel/models.py
@@ -44,6 +44,7 @@ class Product(db.Model):
     address = Column(String(100), nullable=False)
     distance = Column(String(10), nullable=False)
     description = Column(String(100), nullable=False)
+    rating = Column(Integer, nullable=False)
     price = Column(String(50), default=0)
     image = Column(String(300), default="https://th.bing.com/th/id/OIP.TD3qZlPaZtEM3dkXOP7f2gHaE7?rs=1&pid=ImgDetMain")
     category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
@@ -53,52 +54,28 @@ class Product(db.Model):
         return self.name
 
 
-class Rating(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    score = Column(Float, nullable=False)
-    product_id = Column(Integer, ForeignKey(Product.id), nullable=False)
-    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
-    product = relationship('Product', backref='rating', lazy=True)
-    user = relationship('User', backref='rating', lazy=True)
-
-    def __str__(self):
-        return self.score
-
-
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
-        c1 = Category(name="Phòng VIP")
-        c2 = Category(name="Phòng Đôi")
-        c3 = Category(name="Phòng Đơn")
-        l1 = Location(name="TP Hồ Chí Minh")
-        l2 = Location(name="Hà Nội")
-        l3 = Location(name="Vũng Tàu")
-        db.session.add_all([c1, c2, c3, l1, l2, l3])
-        db.session.commit()
-        import json
-        # Lấy category và location bằng cách sử dụng session.get() thay vì query.get()
-        with open('data/products.json', encoding='utf-8') as f:
-            products = json.load(f)
-            for p in products:
-                category = db.session.get(Category, p['category_id'])  # Sử dụng session.get()
-                location = db.session.get(Location, p['location_id'])  # Sử dụng session.get()
-                prod = Product(
-                    name=p['name'],
-                    address=p['address'],
-                    distance=p['distance'],
-                    description=p['description'],
-                    price=p['price'],
-                    image=p['image'],
-                    category=category,  # Sử dụng đối tượng Category thay vì ID
-                    location=location  # Sử dụng đối tượng Location thay vì ID
-                )
-                db.session.add(prod)
-
-        db.session.commit()
-
-        import hashlib
-        u = User(name="KieuThanhDuc", phone="0987654321", username="admin", password= str(hashlib.md5("123".encode('utf-8')).hexdigest()))
-        db.session.add(u)
-        db.session.commit()
-        # pass
+        # db.create_all()
+        # c1 = Category(name="Phòng VIP")
+        # c2 = Category(name="Phòng Đôi")
+        # c3 = Category(name="Phòng Đơn")
+        # l1 = Location(name="TP Hồ Chí Minh")
+        # l2 = Location(name="Hà Nội")
+        # l3 = Location(name="Vũng Tàu")
+        # db.session.add_all([c1, c2, c3, l1, l2, l3])
+        # db.session.commit()
+        # import json
+        # with open('data/products.json', encoding='utf-8') as f:
+        #     products = json.load(f)
+        #     for p in products:
+        #         prod = Product(**p)
+        #         db.session.add(prod)
+        #
+        # db.session.commit()
+        #
+        # import hashlib
+        # u = User(name="KieuThanhDuc", phone="0987654321", username="admin", password= str(hashlib.md5("123".encode('utf-8')).hexdigest()))
+        # db.session.add(u)
+        # db.session.commit()
+        pass
